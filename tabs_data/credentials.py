ef6d6a657b883061829763173f7e8c994f533fea
rutuja031 <rutuja.kapratwar@strateena.com>
2025-08-24 17:59:36 +0530

    Update credentials.py

--- tabs_data/credentials.py
+++ tabs_data/credentials.py
@@ -1,24 +1,57 @@
-def cred():
-    from sqlalchemy import create_engine
-    import urllib
+# def cred():
+#     from sqlalchemy import create_engine
+#     import urllib
     
+#     DB_CONFIG = {
+#         'dbname': 'new_db',
+#         'user': 'postgres',
+#         'password': 'Strateena@check',
+#         'host': '34.93.35.170',
+#         'port': 5432
+#     }
+    
+#     def get_engine():
+#         password = urllib.parse.quote_plus(DB_CONFIG['password'])
+#         url = (
+#             f"postgresql+psycopg2://{DB_CONFIG['user']}:{password}"
+#             f"@{DB_CONFIG['host']}:{DB_CONFIG['port']}/{DB_CONFIG['dbname']}"
+#         )
+#         return create_engine(url)
+        
+#     return get_engine()
+
+from google.cloud.sql.connector import Connector
+import sqlalchemy
+
+def cred():
+    # Initialize the Cloud SQL connector
+    connector = Connector()
+
+    # Define connection parameters
     DB_CONFIG = {
+        'instance_connection_name': 'aiml-ho-arg:asia-south1:newdb-demo1234', 
         'dbname': 'new_db',
         'user': 'postgres',
-        'password': 'Strateena@check',
-        'host': '34.93.35.170',
-        'port': 5432
+        'password': 'Strateena@check'
     }
-    
-    def get_engine():
-        password = urllib.parse.quote_plus(DB_CONFIG['password'])
-        url = (
-            f"postgresql+psycopg2://{DB_CONFIG['user']}:{password}"
-            f"@{DB_CONFIG['host']}:{DB_CONFIG['port']}/{DB_CONFIG['dbname']}"
+
+    # Function to create a secure connection
+    def getconn():
+        return connector.connect(
+            DB_CONFIG['instance_connection_name'],
+            "pg8000",
+            user=DB_CONFIG['user'],
+            password=DB_CONFIG['password'],
+            db=DB_CONFIG['dbname']
         )
-        return create_engine(url)
-        
-    return get_engine()
+
+    # Create SQLAlchemy engine using the connector
+    engine = sqlalchemy.create_engine(
+        "postgresql+pg8000://",
+        creator=getconn,
+    )
+
+    return engine
 
 import psycopg2
 from psycopg2.extras import RealDictCursor
